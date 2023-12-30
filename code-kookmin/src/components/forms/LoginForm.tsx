131f086e6b8a23bb61e872b96255f7f25015f1f8
suwith <suwith@kookmin.ac.kr>
2023-12-31 06:43:06 +0900

    [feat] 로그인페이지 구현

---
+++ code-kookmin/src/components/forms/LoginForm.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+import Cookies from 'js-cookie';
+import '../../styles/LoginForm.css';
+import MainButton from '../common/mainButton';
+
+interface LoginFormProps {
+    onSubmit: (values: LoginFormValues) => void;
+}
+
+interface LoginFormValues {
+    userId: string;
+    password: string;
+    rememberMe: boolean;
+}
+
+const LoginFormSchema = Yup.object({
+    userId: Yup.string().required('아이디를 입력하세요'),
+    password: Yup.string().required('비밀번호를 입력하세요'),
+});
+
+const LoginForm: React.FC<LoginFormProps> = ({ onSubmit }) => {
+    const formik = useFormik<LoginFormValues>({
+        initialValues: {
+            userId: '',
+            password: '',
+            rememberMe: false,
+        },
+        validationSchema: LoginFormSchema,
+        onSubmit: (values) => {
+            //로그인 로직 구현
+
+            if (values.rememberMe) {
+                // 로그인 정보를 쿠키에 저장
+                Cookies.set('userData', JSON.stringify({ userId: values.userId }));
+            }
+            onSubmit(values);
+        },
+    });
+
+    return (
+        <form onSubmit={formik.handleSubmit} className="login-form">
+            <div>
+                <label htmlFor="userId">아이디</label>
+                <input
+                    type="text"
+                    id="userId"
+                    name="userId"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.userId}
+                />
+                {formik.touched.userId && formik.errors.userId ? (
+                    <div className="error-message">{formik.errors.userId}</div>
+                ) : null}
+            </div>
+
+            <div>
+                <label htmlFor="password">비밀번호</label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.password}
+                />
+                {formik.touched.password && formik.errors.password ? (
+                    <div className="error-message">{formik.errors.password}</div>
+                ) : null}
+            </div>
+
+            <div className="remember-me">
+                <input
+                    type="checkbox"
+                    id="rememberMe"
+                    name="rememberMe"
+                    onChange={formik.handleChange}
+                    checked={formik.values.rememberMe}
+                />
+                <label htmlFor="rememberMe">로그인 유지</label>
+            </div>
+            <div className="forgot-password">
+                <a href="/forgot-password">비밀번호를 잃어버리셨나요?</a>
+            </div>
+
+            <MainButton onClick={() => formik.handleSubmit()} className="login-button">
+                로그인
+            </MainButton>
+        </form>
+    );
+};
+
+export default LoginForm;
