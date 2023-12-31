2997a0c1478962a745b056bc318d0295561e446c
suwith <suwith@kookmin.ac.kr>
2023-12-31 09:44:29 +0900

    [feat] 회원가입 구현

--- code-kookmin/src/App.tsx
+++ code-kookmin/src/App.tsx
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/homePage';
 import LoginPage from './pages/loginPage';
 import FindPWPage from './pages/findPWPage';
+import SignupPage from './pages/signupPage';
 
 function App() {
     return (
@@ -15,6 +16,7 @@ function App() {
                         <Route path="/" element={<HomePage />} />
                         <Route path="/login" element={<LoginPage />} />
                         <Route path="/findPW" element={<FindPWPage />} />
+                        <Route path="/signup" element={<SignupPage />} />
                     </Routes>
                 </Layout>
             }
